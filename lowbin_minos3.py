485522a770ecd419a3e3e5f9a42a8f1589a88310
sensei <sensei@jiji.fnal.gov>
2022-05-31 10:37:38 -0500

    all scripts

--- lowbin_minos3.py
+++ lowbin_minos3.py
@@ -19,6 +19,18 @@ badFlags_2e = { 'badColM', 'badPixM', 'bleedM', 'crosstalkM', 'edgeM',
 }
 
 
+
+quads = [0,1,2,3]
+quads = [0]
+minos_halorad = []
+# pix_sizes = np.arange(10, 110, 5)
+pix_sizes = np.arange(0, 10, 1)
+pix_sizes = [2,5,10]
+# pix_sizes = [10]
+ccdnum = int(sys.argv[1])
+enum = int(sys.argv[2])
+lowfix = int(sys.argv[3])
+
 prfs = ['/data/users/meeg/minos3/proc_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run1_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE72000_CLEAR1800_1_250.fits']
 #        '/data/users/meeg/minos3/proc_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run1_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE72000_CLEAR1800_1_255.fits',
 #        '/data/users/meeg/minos3/proc_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run1_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE72000_CLEAR1800_1_260.fits',
@@ -31,17 +43,6 @@ mrfs = ['/data/users/meeg/minos3/cuts_new/mask_corr_proc_skp_moduleC40_41-ssc16_
 #        '/data/users/meeg/minos3/cuts_new/mask_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run1_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE72000_CLEAR1800_1_260.fits',
 #        '/data/users/meeg/minos3/cuts_new/mask_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run1_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE72000_CLEAR1800_1_265.fits']
         # '/data/users/meeg/minos3/cuts_new/mask_corr_proc_skp_moduleC40_41-ssc16_17-lta20_60_TEMP135K-run20_NROW520_NBINROW1_NCOL3200_NBINCOL1_EXPOSURE0_CLEAR1800_1_233.fits']
-
-# quads = [0,1,2,3]
-quads = [0]
-minos_halorad = []
-# pix_sizes = np.arange(0, 80, 1)
-# pix_sizes = [0,5,10,15, 60]
-pix_sizes = [10]
-pix_sizes = [2,5,10]
-ccdnum = int(sys.argv[1])
-enum = int(sys.argv[2])
-
 
 hotcols = np.load(f'../data/badcols_{ccdnum}.npy', allow_pickle=True)
 hotpix = np.load(f'../data/badpix_{ccdnum}.npy', allow_pickle=True)
@@ -68,14 +69,19 @@ for q, tr in zip(quads, minos_halorad):
         "fractional2s" : True #Shoudl we calcualte the number of 2s passing the cut, or add the probability?
     }
     tr.df.set_bp(newBp)
+
     # prf = 'proc_corr_proc_skp.*EXPOSURE%i.*_%i_.*.fits' % (enum, ccdnum)
     # mrf = 'mask_corr_proc_skp.*EXPOSURE%i.*_%i_.*.fits' % (enum, ccdnum)
-    tr.df.add_procs(prfs)
-    tr.df.add_masks(mrfs)
     # tr.df.get_fits('/data/users/meeg/minos3/', prf)
     # tr.df.get_maskfits('/data/users/meeg/minos3/cuts_new/', mrf)
+    # tr.df.proc_convert()
+    # tr.df.mfit_convert()
+
+    tr.df.add_procs(prfs)
+    tr.df.add_masks(mrfs)
     tr.df.proc_convert()
     tr.df.mfit_convert()
+
     hpixval = tr.df.maskFlags['badPixM']
     hcolval = tr.df.maskFlags['badColM']
     _ = tr.df.add_hotpixel(tr.df.mfs_full, hotpix[q], hpixval)
@@ -93,15 +99,17 @@ plen = len(pix_sizes)
 flen = len(tr.df.procfits)
 mlen = len(minos_halorad)
 
+e_bins = [380, 22e3]
+blen = len(e_bins)
+
 reduced_pixels = np.zeros((mlen, plen, 2))
-# reduced_counts = np.zeros((mlen, plen, 4))
-reduced_counts = np.zeros((mlen, plen, 8))
+reduced_counts = np.zeros((mlen, plen, 4))
 reduced_expos = np.zeros_like(reduced_pixels)
 
 
-
-
-
+print('--------------------------------------------')
+print(f"Doing fixed low radius with {lowfix}")
+print('--------------------------------------------')
 
 hval = minos_halorad[0].df.maskFlags['haloM']
 mval = minos_halorad[0].df.maskFlags['edgeM']
@@ -110,12 +118,11 @@ for i,m in enumerate(minos_halorad):
 #    _ = m.df.remove_maskval(m.df.mfs, mval) # Clear edge mask
 #    _ = m.df.add_edgemask(m.df.mfs, mval, 60) # Insert new edge mask
     for j in range(len(m.df.sfs)):
-        pixels, counts, expos = m.cluster_halos(m.df.sfs[j], m.df.mfs[j], pix_sizes, include=True)
-        reduced_pixels[i,:,:] += pixels
-        reduced_counts[i,:,:] += counts
-        reduced_expos[i,:,:] += expos
+        pixels, counts, expos = m.conv_cherenkov_low(m.df.sfs[j], m.df.mfs[j], [380, 22e3, -1], pix_sizes, lowrad=lowfix, include=True)
+        reduced_pixels[i,:,:] += pixels[0]
+        reduced_counts[i,:,:] += counts[0]
+        reduced_expos[i,:,:] += expos[0]
         print(reduced_counts[i,:4,:])
-
 
 print('------------------------')
 print(pix_sizes)
@@ -125,6 +132,6 @@ print(reduced_pixels)
 print('------------------------')
 suffix = '_' + str(enum) + '_' + str(ccdnum)
 sys.exit()
-# np.save(f'./data/minos3/channel_expos_1e' + suffix, reduced_expos)
-# np.save(f'./data/minos3/channel_count_1e' + suffix, reduced_counts)
-# np.save(f'./data/minos3/channel_pixel_1e' + suffix, reduced_pixels)
+np.save(f'../data/minos3/{lowfix}/lowbin_expos_1e' + suffix, reduced_expos)
+np.save(f'../data/minos3/{lowfix}/lowbin_count_1e' + suffix, reduced_counts)
+np.save(f'../data/minos3/{lowfix}/lowbin_pixel_1e' + suffix, reduced_pixels)
