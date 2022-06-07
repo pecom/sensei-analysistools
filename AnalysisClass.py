00b6775328cc21dcbecc246a4bc64e1696a3f603
Prak <8682372+pecom@users.noreply.github.com>
2022-06-07 11:33:54 -0400

    poisson dictionary for efficiency

--- AnalysisClass.py
+++ AnalysisClass.py
@@ -806,7 +806,17 @@ class Analysis:
             count_errs[1,i] = ci[1]
         return count_errs
 
-
+    def poiss_upperlimit(count, perc=.9):
+        # Gamma function for Poisson upper limit calculations
+        gamma = lambda k, ts: ts**k * np.exp(-ts)
+        tmax = 50
+        deltaT = .01
+        ts1 = np.arange(0,tmax, deltaT)
+        g1 = gamma(count, ts1)
+        rhs = np.cumsum(g1 * deltaT)
+        lhs = perc * np.math.factorial(count)
+        h1 = ts1[np.argmin(np.abs(rhs - lhs)) + 1]
+        return h1
 
     # Calculate the upper limits given counts and percentage
     def create_upperlimit(self, counts, perc=.90):
@@ -814,19 +824,18 @@ class Analysis:
         norm_perc = perc + (1-perc)/2
         sig = stats.norm.ppf(norm_perc, 0, 1)
 
-        # Gamma function for Poisson upper limit calculations
-        gamma = lambda k, ts: ts**k * np.exp(-ts)
-        tmax = 50
-        deltaT = .01
-        ts1 = np.arange(0,tmax, deltaT)
+        if self.poisson is not None:
+            continue
+        else:
+            poiss = {}
+            for i in range(31):
+                poiss[i] = poiss_upperlimit(i)
+            self.poisson = poiss
         for i,k in enumerate(counts):
             if k > 30:
                 h1 = ((sig + np.sqrt(sig**2 + 4*k))/2)**2
             else:
-                g1 = gamma(k, ts1)
-                rhs = np.cumsum(g1 * deltaT)
-                lhs = perc * np.math.factorial(k)
-                h1 = ts1[np.argmin(np.abs(rhs - lhs)) + 1]
+                h1 = poiss[k]
             count_UL[i] = h1
 
         return count_UL
